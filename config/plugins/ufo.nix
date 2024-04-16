3cc58488ea4d6a3be95d623d3099e36c6029ce52
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-04-17 00:13:35 +0100

    Use plugins.lsp.capabilities for ufo capability addition

--- config/plugins/ufo.nix
+++ config/plugins/ufo.nix
@@ -36,20 +36,13 @@
     '';
   };
 
-  extraConfigLua =
+  plugins.lsp.capabilities =
     # lua
     ''
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities.textDocument.foldingRange = {
           dynamicRegistration = false,
           lineFoldingOnly = true
       }
-      local language_servers = require('lspconfig').util.available_servers()
-      for _, ls in ipairs(language_servers) do
-          require('lspconfig')[ls].setup({
-              capabilities = capabilities
-          })
-      end
     '';
 
   keymaps = [
