345f5a8b5a414e7a41f5e2aab8a945f746117883
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-09-04 14:27:11 +0100

    Integrate keymap comments into options.desc options

--- modules/keymaps.nix
+++ modules/keymaps.nix
@@ -1,210 +1,184 @@
 {
   keymaps = [
-    # Quit vim
     {
       key = "<leader>q";
       action = "<cmd>qa<cr>";
       mode = "n";
+      options.desc = "Quit nvim";
     }
-    # Force quit
     {
       key = "<leader>Q";
       action = "<cmd>qa!<cr>";
+      options.desc = "Force quit nvim";
     }
-
-    # Write all open files
     {
       key = "<leader>w";
       action = "<cmd>wa<cr>";
       mode = "n";
+      options.desc = "Write all open files";
     }
-    # Write all open files and quit
     {
       key = "<leader>W";
       action = "<cmd>wqa<cr>";
       mode = "n";
+      options.desc = "Write all open files and quit";
     }
-
-    # Write current file
     {
       key = "<leader><leader>";
       action = "<cmd>w<cr>";
       mode = "n";
+      options.desc = "Write current file";
     }
-
-    # Easier directional navigation between panes
     {
       key = "<C-j>";
       action = "<C-w>j";
       mode = "n";
+      options.desc = "Move to pane below";
     }
     {
       key = "<C-k>";
       action = "<C-w>k";
       mode = "n";
+      options.desc = "Move to pane above";
     }
     {
       key = "<C-h>";
       action = "<C-w>h";
       mode = "n";
+      options.desc = "Move to pane to the left";
     }
     {
       key = "<C-l>";
       action = "<C-w>l";
       mode = "n";
+      options.desc = "Move to pane to the right";
     }
-
-    # Easier splitting into new panes
-    # Open empty buffer by default
     {
       key = "<Bar>";
       action = "<C-w>v<C-w>l<C-w>n<C-w>j<C-w>c";
       mode = "n";
+      options.desc = "Open empty buffer in vsplit";
     }
     {
       key = "-";
       action = "<C-w>n<C-w>x<C-w>j";
       mode = "n";
+      options.desc = "Open empty buffer in split";
     }
-
-    # Open current file with leader
     {
       key = "<leader><Bar>";
       action = "<C-w>v<C-w>l";
       mode = "n";
+      options.desc = "Open current buffer in vsplit";
     }
     {
       key = "<leader>-";
       action = "<C-w>s<C-w>j";
       mode = "n";
+      options.desc = "Open current buffer in split";
     }
-
-    # Run current file
     {
       key = "<leader>r";
       action = ''<cmd>!"%:p"<cr>'';
       mode = "n";
+      options.desc = "Run the current file (requires shebang)";
     }
-
-    # Clear the terminal and run the current file
     {
       key = "<leader>R";
       action = ''<cmd>!clear<cr><cr><cmd>"%:p"<cr>'';
       mode = "n";
+      options.desc = "Clear the terminal and run the current file (requires shebang)";
     }
-
-    # Run current file with arguments
     {
       key = "<leader>A";
       action = ''<cmd>!"%:p" '';
       mode = "n";
+      options.desc = "Run the current file with arguments (requires shebang)";
     }
-
-    # Duplicate the current line
     {
       key = "<C-d>";
       action = "yyp";
       mode = "n";
+      options.desc = "Duplicate current line";
     }
-
-    # Don't skip wrapped lines with arrow keys
     {
       key = "<up>";
-      action = "g<up>";
+      action = "gk";
       mode = "n";
+      options.desc = "Move one line up (ignores wrapping)";
     }
     {
       key = "<down>";
-      action = "g<down>";
+      action = "gj";
       mode = "n";
+      options.desc = "Move one line down (ignores wrapping)";
     }
-
-    # Clear the terminal
-    {
-      key = "<leader>cl";
-      action = "<cmd>!clear<cr><cr>";
-      mode = "n";
-    }
-
-    # Remove search highlight
-    {
-      key = "<leader>cl";
-      action = "<cmd>!clear<cr><cr>";
-      mode = "n";
-      options.silent = true;
-    }
-
-    # Add a semicolon or comma to the end of a line
     {
       key = "<leader>;";
       action = "mqA;<esc>`qdmq";
       mode = "n";
+      options.desc = "Add a semicolon to the end of the line";
     }
     {
       key = "<leader>,";
       action = "mqA,<esc>`qdmq";
       mode = "n";
+      options.desc = "Add a comma to the end of the line";
     }
-
-    # Add a colon to the end of the line and start a new line
     {
       key = "<leader>:";
       action = "A:<esc>o";
       mode = "n";
+      options.desc = "Add a colon to the end of the line and start a new line";
     }
-
-    # Always use very magic regex mode
     {
       key = "/";
       action = "/\\v";
       mode = "n";
+      options.desc = "Always use very magic regex mode";
     }
-
-    # Turn off highlighting after a search
     {
       key = "<leader>n";
       action = "<cmd>nohlsearch<cr>";
       mode = "n";
+      options.desc = "Remove search highlight";
     }
-
-    # Toggle spell check
     {
       key = "<leader>s";
       action = "<cmd>set spell!<cr>";
       mode = "n";
-      options.silent = true;
+      options = {
+        silent = true;
+        desc = "Toggle spell check";
+      };
     }
-
-    # Automatically fix spelling
     {
       key = "<leader>z";
       action = "mq1z=`qdmq";
       mode = "n";
-      options.silent = true;
+      options = {
+        silent = true;
+        desc = "Automatically fix spelling";
+      };
     }
-
-    # Copy whole buffer to system clipboard
     {
       key = "<leader>cp";
       action = "<cmd>w !xclip -selection c<cr><cr>";
       mode = "n";
+      options.desc = "Copy whole buffer to system clipboard";
     }
-
-    # Copy selected text to system clipboard
     {
       key = "<leader>cp";
       action = ''"+y'';
       mode = "v";
+      options.desc = "Copy selected text to system clipboard";
     }
-
-    # Select everything in the buffer
     {
       key = "<leader>a";
       action = "ggVG";
       mode = "n";
+      options.desc = "Select everything in the buffer";
     }
-
-    # Toggle virtual text
     {
       key = "<leader>v";
       action.__raw =
@@ -216,10 +190,11 @@
           end
         '';
       mode = "n";
-      options.silent = true;
+      options = {
+        silent = true;
+        desc = "Toggle virtual text";
+      };
     }
-
-    # Toggle inlay hints
     {
       key = "<leader>i";
       action.__raw =
@@ -230,7 +205,10 @@
           end
         '';
       mode = "n";
-      options.silent = true;
+      options = {
+        silent = true;
+        desc = "Toggle inlay hints";
+      };
     }
   ];
 }
