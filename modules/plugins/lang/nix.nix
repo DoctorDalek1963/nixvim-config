a1c4b785758c32e8a32612874645f0ec24b2bfb4
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-09-16 00:26:40 +0100

    Do Nix formatting with none-ls instead of with nixd

--- modules/plugins/lang/nix.nix
+++ modules/plugins/lang/nix.nix
@@ -9,12 +9,11 @@ in {
     plugins = {
       lsp.servers.nixd = {
         enable = true;
-        # Why does Alejandra not work? I have no idea.
-        # When I save one buffer, it formats it and overwrites any other open
-        # nix buffers with the same formatted code. This is obviously very
-        # annoying and I don't know why it's happening, so I'm disabling
-        # Alejandra for a while.
-        # settings.formatting.command = ["${pkgs.alejandra}/bin/alejandra" "--quiet"];
+        # It seems like something in my config is stopping alejandra playing
+        # nice with nixd. When I save one buffer, it formats it and overwrites
+        # any other open nix buffers with the same formatted code. This is
+        # obviously very annoying and I don't know why it's happening, so I'm using none-ls to do all my alejandra formatting.
+        settings.formatting.command = null;
       };
 
       none-ls.sources = {
@@ -24,6 +23,9 @@ in {
         diagnostics = {
           deadnix.enable = true;
           statix.enable = true;
+        };
+        formatting = {
+          alejandra.enable = true;
         };
       };
     };
