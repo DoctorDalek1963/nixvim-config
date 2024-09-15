79a0808d6844f1d7d22e4e0d79cc4478464231c3
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-09-16 00:03:35 +0100

    Revert "Use nixfmt-rfc-style instead of alejandra"
    This reverts commit 24cdf3c8e679b564de14ed3cacd29df06f453c61.

--- modules/plugins/lang/nix.nix
+++ modules/plugins/lang/nix.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   lib,
   config,
   ...
@@ -10,7 +9,12 @@ in {
     plugins = {
       lsp.servers.nixd = {
         enable = true;
-        settings.formatting.command = [ "${pkgs.nixfmt-rfc-style}/bin/nixfmt" ];
+        # Why does Alejandra not work? I have no idea.
+        # When I save one buffer, it formats it and overwrites any other open
+        # nix buffers with the same formatted code. This is obviously very
+        # annoying and I don't know why it's happening, so I'm disabling
+        # Alejandra for a while.
+        # settings.formatting.command = ["${pkgs.alejandra}/bin/alejandra" "--quiet"];
       };
 
       none-ls.sources = {
