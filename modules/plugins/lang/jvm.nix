495540fa308fe271044acc567523947d231fb95f
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-01-20 11:39:44 +0000

    Use nvim-jdtls instead of java_language_server for JVM

--- modules/plugins/lang/jvm.nix
+++ modules/plugins/lang/jvm.nix
@@ -6,11 +6,17 @@
   cfg = config.setup.lang;
 in {
   config = lib.mkIf (cfg.enable && cfg.jvm) {
-    plugins.lsp.servers = {
-      clojure_lsp.enable = true;
-      java_language_server.enable = true;
-      kotlin_language_server.enable = true;
-      metals.enable = true; # Scala
+    plugins = {
+      lsp.servers = {
+        clojure_lsp.enable = true;
+        kotlin_language_server.enable = true;
+        metals.enable = true; # Scala
+      };
+
+      nvim-jdtls = {
+        enable = true;
+        data = "/home/dyson/.cache/jdtls/workspaceData";
+      };
     };
   };
 }
