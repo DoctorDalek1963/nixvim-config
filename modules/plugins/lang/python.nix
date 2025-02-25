cbb1ee1cbab81604e220adca9288183956d696eb
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-02-25 18:44:23 +0000

    Fix and re-enable pylsp

--- modules/plugins/lang/python.nix
+++ modules/plugins/lang/python.nix
@@ -7,15 +7,35 @@
 in {
   config = lib.mkIf (cfg.enable && cfg.python) {
     plugins.lsp.servers.pylsp = {
-      enable = false;
+      enable = true;
       settings.plugins = {
-        autopep8.enabled = true;
-        flake8.enabled = true;
-        pycodestyle.enabled = true;
-        pydocstyle.enabled = true;
-        pylsp_mypy.enabled = true;
-        rope.enabled = true;
-        ruff.enabled = true;
+        # Linting
+        pycodestyle = {
+          enabled = true;
+          maxLineLength = 120;
+        };
+        pyflakes.enabled = true;
+        flake8.enabled = false; # Conflicts with pycodestyle/pyflakes combo
+
+        # FIXME: Currently causes the build to fail
+        # pylsp_mypy.enabled = true;
+
+        # Formatting
+        black.enabled = true;
+        isort.enabled = true;
+        yapf.enabled = false;
+
+        # Completion
+        jedi_completion = {
+          enabled = true;
+          fuzzy = true;
+        };
+        jedi_definition.enabled = true;
+
+        # Rope conflicts with Jedi and creates .ropeproject/ in repos
+        pylsp_rope.enabled = false;
+        rope_autoimport.enabled = false;
+        rope_completion.enabled = false;
       };
     };
   };
