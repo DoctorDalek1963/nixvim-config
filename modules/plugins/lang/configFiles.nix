5edd887461de6d37182bb11933d35ccb40886b14
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-05-30 16:18:33 +0100

    Add just LSP

--- modules/plugins/lang/configFiles.nix
+++ modules/plugins/lang/configFiles.nix
@@ -7,6 +7,7 @@
 in {
   config = lib.mkIf (cfg.enable && cfg.configFiles) {
     plugins.lsp.servers = {
+      just.enable = true;
       jsonls.enable = true;
       taplo.enable = true; # TOML
       yamlls.enable = true;
