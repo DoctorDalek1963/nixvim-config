4e27cfbe2e0f798095e9ff79aa17c0c88edd82cb
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-06-03 18:09:12 +0100

    Add oil-git-status

--- modules/plugins/oil.nix
+++ modules/plugins/oil.nix
@@ -4,27 +4,33 @@
   ...
 }: {
   config = lib.mkIf config.setup.pluginGroups.niceToHave {
-    plugins.oil = {
-      enable = true;
-      settings = {
-        default_file_explorer = true;
-        experimental_watch_for_changes = true;
-        prompt_save_on_select_new_entry = true;
-        skip_confirm_for_simple_edits = false;
-        view_options.show_hidden = true;
+    plugins = {
+      oil = {
+        enable = true;
+        settings = {
+          default_file_explorer = true;
+          experimental_watch_for_changes = true;
+          prompt_save_on_select_new_entry = true;
+          skip_confirm_for_simple_edits = false;
+          view_options.show_hidden = true;
 
-        columns = [
-          {
-            __unkeyed = "size";
-            highlight = "Special";
-          }
-          "icon"
-        ];
+          win_options.signcolumn = "auto:1-2";
 
-        keymaps = {
-          ";" = "actions.select";
+          columns = [
+            {
+              __unkeyed = "size";
+              highlight = "Special";
+            }
+            "icon"
+          ];
+
+          keymaps = {
+            ";" = "actions.select";
+          };
         };
       };
+
+      oil-git-status.enable = true;
     };
 
     keymaps = [
