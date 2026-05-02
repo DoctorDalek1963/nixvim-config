8a846261ce5f027264d3b0d8167be80da86e9b98
DoctorDalek1963 <dyson.dyson@icloud.com>
2026-05-02 19:41:37 +0100

    Use filters and maps to make packages and checks programmatically

--- modules/flake/nixvim.nix
+++ modules/flake/nixvim.nix
@@ -23,28 +23,18 @@
       ...
     }:
     let
-      inherit (inputs.nixvim.lib) evalNixvim;
-      inherit (inputs.nixvim.lib.${system}.check) mkTestDerivationFromNixvimModule;
-
-      evalNixvimModule =
-        module:
-        evalNixvim {
-          inherit system;
-          modules = [ module ];
-        };
+      nvim-modules = lib.filterAttrs (
+        name: _: (builtins.substring 0 5 name) == "nvim-"
+      ) self.nixvimModules;
     in
     {
-      nixvimConfigurations = {
-        nvim-tiny = evalNixvimModule self.nixvimModules.nvim-tiny;
-        nvim-small = evalNixvimModule self.nixvimModules.nvim-small;
-        nvim-medium = evalNixvimModule self.nixvimModules.nvim-medium;
-        nvim-full = evalNixvimModule self.nixvimModules.nvim-full;
-
-        nvim-tiny-nightly = evalNixvimModule self.nixvimModules.nvim-tiny-nightly;
-        nvim-small-nightly = evalNixvimModule self.nixvimModules.nvim-small-nightly;
-        nvim-medium-nightly = evalNixvimModule self.nixvimModules.nvim-medium-nightly;
-        nvim-full-nightly = evalNixvimModule self.nixvimModules.nvim-full-nightly;
-      };
+      nixvimConfigurations = builtins.mapAttrs (
+        _: module:
+        inputs.nixvim.lib.evalNixvim {
+          inherit system;
+          modules = [ module ];
+        }
+      ) nvim-modules;
 
       packages =
         (lib.mapAttrs (_: conf: conf.config.build.package) self.nixvimConfigurations.${system})
@@ -52,32 +42,11 @@
           default = self'.packages.nvim-medium;
         };
 
-      checks = {
-        nvim-tiny = mkTestDerivationFromNixvimModule {
-          module = self.nixvimModules.nvim-tiny;
-        };
-        nvim-small = mkTestDerivationFromNixvimModule {
-          module = self.nixvimModules.nvim-small;
-        };
-        nvim-medium = mkTestDerivationFromNixvimModule {
-          module = self.nixvimModules.nvim-medium;
-        };
-        nvim-full = mkTestDerivationFromNixvimModule {
-          module = self.nixvimModules.nvim-full;
-        };
-
-        nvim-tiny-nightly = mkTestDerivationFromNixvimModule {
-          module = self.nixvimModules.nvim-tiny-nightly;
-        };
-        nvim-small-nightly = mkTestDerivationFromNixvimModule {
-          module = self.nixvimModules.nvim-small-nightly;
-        };
-        nvim-medium-nightly = mkTestDerivationFromNixvimModule {
-          module = self.nixvimModules.nvim-medium-nightly;
-        };
-        nvim-full-nightly = mkTestDerivationFromNixvimModule {
-          module = self.nixvimModules.nvim-full-nightly;
-        };
-      };
+      checks = builtins.mapAttrs (
+        _: module:
+        inputs.nixvim.lib.${system}.check.mkTestDerivationFromNixvimModule {
+          inherit module;
+        }
+      ) nvim-modules;
     };
 }
