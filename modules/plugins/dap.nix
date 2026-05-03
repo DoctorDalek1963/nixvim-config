4230bf2abea3a136f813131a80cbb736c6bba7fc
DoctorDalek1963 <dyson.dyson@icloud.com>
2026-05-03 18:16:18 +0100

    Lazy load DAP

--- modules/plugins/dap.nix
+++ modules/plugins/dap.nix
@@ -2,75 +2,104 @@
   flake.nixvimModules.dap = {
     # Debug Adapter Protocol
     plugins = {
-      dap.enable = true;
-      dap-ui.enable = true;
-      dap-virtual-text.enable = true;
+      dap = {
+        enable = true;
 
-      cmp.settings.sources = [ { name = "dap"; } ];
-    };
+        lazyLoad.settings.keys = [
+          "<leader>db"
+          "<leader>dc"
+          "<leader>dso"
+          "<leader>dsi"
+          "<leader>dr"
+        ];
 
-    keymaps = [
-      {
-        key = "<leader>db";
-        action.__raw = "require('dap').toggle_breakpoint";
-        mode = "n";
-        options = {
-          silent = true;
-          desc = "Toggle DAP breakpoint";
-        };
-      }
-      {
-        key = "<leader>dc";
-        action.__raw = "require('dap').continue";
-        mode = "n";
-        options = {
-          silent = true;
-          desc = "DAP continue";
-        };
-      }
-      {
-        key = "<leader>dso";
-        action.__raw = "require('dap').step_over";
-        mode = "n";
-        options = {
-          silent = true;
-          desc = "DAP step over";
-        };
-      }
-      {
-        key = "<leader>dsi";
-        action.__raw = "require('dap').step_into";
-        mode = "n";
-        options = {
-          silent = true;
-          desc = "DAP step into";
-        };
-      }
-      {
-        key = "<leader>dr";
-        action.__raw = "require('dap').repl.open";
-        mode = "n";
-        options = {
-          silent = true;
-          desc = "Open DAP REPL";
-        };
-      }
-      {
-        key = "<leader>dui";
-        action.__raw =
-          # lua
-          ''
+        luaConfig.post = ''
+          require('lz.n').trigger_load('nvim-dap-virtual-text')
+        '';
+      };
+
+      dap-virtual-text = {
+        enable = true;
+        lazyLoad.settings.lazy = true; # Trigger manually from dap
+      };
+
+      dap-ui = {
+        enable = true;
+
+        lazyLoad.settings = {
+          before.__raw = ''
             function()
-              require('dapui').setup()
-              require('dapui').toggle()
+              require('lz.n').trigger_load('nvim-dap')
             end
           '';
-        mode = "n";
-        options = {
-          silent = true;
-          desc = "Open DAP UI";
+
+          keys = [ "<leader>dui" ];
         };
-      }
-    ];
+      };
+
+      lz-n.keymaps = [
+        {
+          plugin = "nvim-dap";
+          key = "<leader>db";
+          action.__raw = "function() require('dap').toggle_breakpoint() end";
+          mode = "n";
+          options = {
+            silent = true;
+            desc = "Toggle DAP breakpoint";
+          };
+        }
+        {
+          plugin = "nvim-dap";
+          key = "<leader>dc";
+          action.__raw = "function() require('dap').continue() end";
+          mode = "n";
+          options = {
+            silent = true;
+            desc = "DAP continue";
+          };
+        }
+        {
+          plugin = "nvim-dap";
+          key = "<leader>dso";
+          action.__raw = "function() require('dap').step_over() end";
+          mode = "n";
+          options = {
+            silent = true;
+            desc = "DAP step over";
+          };
+        }
+        {
+          plugin = "nvim-dap";
+          key = "<leader>dsi";
+          action.__raw = "function() require('dap').step_into() end";
+          mode = "n";
+          options = {
+            silent = true;
+            desc = "DAP step into";
+          };
+        }
+        {
+          plugin = "nvim-dap";
+          key = "<leader>dr";
+          action.__raw = "function() require('dap').repl.open() end";
+          mode = "n";
+          options = {
+            silent = true;
+            desc = "Open DAP REPL";
+          };
+        }
+
+        {
+          plugin = "nvim-dap-ui";
+          key = "<leader>dui";
+          action.__raw = "function() require('dapui').toggle() end";
+          mode = "n";
+          options = {
+            silent = true;
+            desc = "Open DAP UI";
+          };
+        }
+      ];
+    };
   };
 }
