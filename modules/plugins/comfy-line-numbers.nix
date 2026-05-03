cb3613ff9ec3e6f0f4d6235e59dd04913420822a
DoctorDalek1963 <dyson.dyson@icloud.com>
2026-05-03 22:34:57 +0100

    Finally fix comfy-line-numbers moving the column!

--- modules/plugins/comfy-line-numbers.nix
+++ modules/plugins/comfy-line-numbers.nix
@@ -1,51 +1,52 @@
 {
   flake.nixvimModules.comfy-line-numbers =
-    { pkgs, lib, ... }:
+    { lib, ... }:
     {
-      # TODO: Work out why signcolumn changes width
-      extraPlugins = [ pkgs.vimPlugins.comfy-line-numbers-nvim ];
+      plugins.comfy-line-numbers = {
+        enable = true;
 
-      extraConfigLua =
-        let
-          maxDigit = 4;
-          labelCount = 80;
+        settings =
+          let
+            maxDigit = 4;
+            labelCount = 80;
 
-          mod' =
-            n:
-            let
-              m = lib.mod n maxDigit;
-            in
-            if m == 0 then maxDigit else m;
+            mod' =
+              n:
+              let
+                m = lib.mod n maxDigit;
+              in
+              if m == 0 then maxDigit else m;
 
-          # It's actually a mixed base system because all non-unit columns can be
-          # zero but the unit column can't. That makes everything weird and freaky,
-          # so we have to define our own toBase function
-          toBase =
-            n:
-            if n < 0 then
-              abort "comfy-line-numbers toBase should never be called with a negative number"
-            else if n == 0 then
-              [ ]
-            else
-              (toBase ((n - 1) / maxDigit)) ++ [ (mod' n) ];
+            # It's actually a mixed base system because all non-unit columns can be
+            # zero but the unit column can't. That makes everything weird and freaky,
+            # so we have to define our own toBase function
+            toBase =
+              n:
+              if n < 0 then
+                abort "comfy-line-numbers toBase should never be called with a negative number"
+              else if n == 0 then
+                [ ]
+              else
+                (toBase ((n - 1) / maxDigit)) ++ [ (mod' n) ];
 
-          labelNums = map toBase (lib.range 1 labelCount);
-          labels = map (xs: lib.concatMapStringsSep "" toString xs) labelNums;
-        in
-        # lua
-        ''
-          require('comfy-line-numbers').setup({
-            labels = { ${lib.concatMapStringsSep ", " (x: "'${x}'") labels} },
-            up_key = 'k',
-            down_key = 'j'
-          })
-        '';
+            labelNums = map toBase (lib.range 1 labelCount);
+          in
+          {
+            labels = map (xs: lib.concatMapStringsSep "" toString xs) labelNums;
 
-      opts.relativenumber = lib.mkForce false;
+            up_key = "k";
+            down_key = "j";
+          };
+      };
+
+      opts = {
+        relativenumber = lib.mkForce false;
+        signcolumn = "yes";
+      };
 
       # Taken from https://github.com/sitiom/nvim-numbertoggle/blob/main/plugin/numbertoggle.lua
       # and adapted to use comfy-line-numbers. This doesn't work when moving between
-      # buffers because comfy-line-numbers applies to all buffers at the  same time,
+      # buffers because comfy-line-numbers applies to all buffers at the same time,
       # so we only care about moving between normal, insert, and command mode
       autoGroups.numbertoggle_augroup = {
         clear = true;
@@ -57,29 +58,37 @@
           pattern = "*";
           event = [
             "BufEnter"
+            "FocusGained"
             "InsertLeave"
             "CmdlineLeave"
+            "WinEnter"
           ];
           callback.__raw = ''
             function()
               if vim.o.number and vim.api.nvim_get_mode().mode ~= "i" then
                 require("comfy-line-numbers").enable_line_numbers()
+                vim.opt.numberwidth = 4
               end
              end
           '';
         }
+
         {
           desc = "Disable comfy line numbers outside of normal mode";
           group = "numbertoggle_augroup";
           pattern = "*";
           event = [
+            "BufLeave"
+            "FocusLost"
             "InsertEnter"
             "CmdlineEnter"
+            "WinLeave"
           ];
           callback.__raw = ''
             function()
               if vim.o.number then
                 require("comfy-line-numbers").disable_line_numbers()
+                vim.opt.numberwidth = 5
 
                 -- Conditional taken from https://github.com/rockyzhang24/dotfiles/commit/03dd14b5d43f812661b88c4660c03d714132abcf
                 -- Workaround for https://github.com/neovim/neovim/issues/32068
