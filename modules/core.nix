5a9b0957b553d26e487fa51ce9f2c4c17f44873d
DoctorDalek1963 <dyson.dyson@icloud.com>
2026-05-15 20:53:35 +0100

    Allow necessary unfree packages

--- modules/core.nix
+++ modules/core.nix
@@ -1,66 +1,77 @@
 { self, ... }:
 {
-  flake.nixvimModules.core = {
-    imports = with self.nixvimModules; [
-      autocmd-groups
-      filetypes
-      keymaps
-      lazy-loading
-      options
-    ];
+  flake.nixvimModules.core =
+    { lib, ... }:
+    {
+      imports = with self.nixvimModules; [
+        autocmd-groups
+        filetypes
+        keymaps
+        lazy-loading
+        options
+      ];
 
-    enableMan = false;
+      # I should be able to use nixpkgs.config.allowUnfreePackages across
+      # multiple modules but that doesn't work for reasons I don't understand
+      nixpkgs.config.allowUnfreePredicate =
+        pkg:
+        builtins.elem (lib.getName pkg) [
+          "blink-cmp-spell"
+          "transparent.nvim"
+        ];
 
-    performance.byteCompileLua = {
-      enable = true;
-      configs = true;
-      initLua = true;
-      nvimRuntime = true;
-      plugins = true;
-    };
+      enableMan = false;
 
-    userCommands = {
-      Rmsp = {
-        desc = "Remove all trailing spaces";
-        command = "execute '%s/\\s\\+$//e'";
+      performance.byteCompileLua = {
+        enable = true;
+        configs = true;
+        initLua = true;
+        nvimRuntime = true;
+        plugins = true;
       };
-    };
 
-    globals = {
-      mapleader = "\\";
-      maplocalleader = "\\";
-    };
-
-    colorschemes.catppuccin = {
-      enable = true;
-      settings = {
-        flavour = "macchiato";
-        background = {
-          light = "latte";
-          dark = "macchiato";
-        };
-        dimInactive = {
-          enabled = true;
-          percentage = 0.15;
-        };
-        integrations = {
-          cmp = true;
-          gitgutter = true;
-          rainbow_delimiters = true;
-          treesitter = true;
-          treesitter_context = true;
-          dap = {
-            enabled = true;
-            enable_ui = true;
-          };
-          indent_blankline = {
-            enabled = true;
-            colored_indent_levels = true;
-          };
-          native_lsp = true;
-          telescope = true;
+      userCommands = {
+        Rmsp = {
+          desc = "Remove all trailing spaces";
+          command = "execute '%s/\\s\\+$//e'";
         };
       };
+
+      globals = {
+        mapleader = "\\";
+        maplocalleader = "\\";
+      };
+
+      colorschemes.catppuccin = {
+        enable = true;
+        settings = {
+          flavour = "macchiato";
+          background = {
+            light = "latte";
+            dark = "macchiato";
+          };
+          dimInactive = {
+            enabled = true;
+            percentage = 0.15;
+          };
+          integrations = {
+            cmp = true;
+            gitgutter = true;
+            rainbow_delimiters = true;
+            treesitter = true;
+            treesitter_context = true;
+            dap = {
+              enabled = true;
+              enable_ui = true;
+            };
+            indent_blankline = {
+              enabled = true;
+              colored_indent_levels = true;
+            };
+            native_lsp = true;
+            telescope = true;
+          };
+        };
+      };
     };
-  };
 }
